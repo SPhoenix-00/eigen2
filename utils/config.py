3498eef6e528c9cfd3fcb912d7dfd799ed73460f
SPhoenix-00 <pj.skirmish@gmail.com>
2025-11-04 01:09:58 -0500

    Hotfix on buffer size

--- utils/config.py
+++ utils/config.py
@@ -67,7 +67,7 @@ class Config:
     # Replay buffer
     BUFFER_SIZE = 12500  # Maximum buffer size
     BATCH_SIZE = 4
-    MIN_BUFFER_SIZE = 5000  # Start training after this many transitions
+    MIN_BUFFER_SIZE = 10000  # Start training after this many transitions
     MIN_BUFFER_SIZE_SWEEP = 5000  # Lower threshold for sweeps (10 gens, faster DDPG)
     
     # Exploration noise
